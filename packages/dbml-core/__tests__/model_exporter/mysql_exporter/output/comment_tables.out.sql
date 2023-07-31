72ae91cddb92fd95e7d74c4f2d805cfccaad13df
huydna <huy.dna@holistics.io>
2023-07-31 12:32:54 +0700

    clean irrelevant files

--- packages/dbml-core/__tests__/model_exporter/mysql_exporter/output/comment_tables.out.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `orders` (
-  `id` int PRIMARY KEY,
-  `user_id` int UNIQUE NOT NULL,
-  `status` varchar(255) COMMENT 'Status of an order',
-  `created_at` varchar(255) COMMENT 'When order created'
-);
